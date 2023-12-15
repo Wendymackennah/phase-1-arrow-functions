75a7dd8fb694005d62da19528ef28a73480becd1
wendymackennah <wendymackennah5@gmail.com>
2023-12-15 12:07:21 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1) Function expression called divide
+const divide = (x='2000',y='100') => x/y;
+
+// 3) Arrow function called square
+const square = x => x * x;
+
+// 5) Arrow function called add
+const add = (a, b) => a + b;
